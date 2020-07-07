09170caae893e7f38fd34103353d9672b1bd69fa
Arnold Caleb <asiimwearnoldcaleb@gmail.com>
2020-07-07 19:44:33 +0300

    Renaming the image files 001

---
+++ hidden.py
@@ -0,0 +1,18 @@
+###file system paths and semantics for different operating systems###
+import os
+from pathlib import Path
+
+'''
+Navigate to the directory containing the files that contain files that need to be renamed
+from the current working directory
+'''
+current_dir = Path.cwd().joinpath('files')
+
+#iterate over the folders that contain the files that need to be renamed
+
+for folder in current_dir.iterdir():
+    for i, f in enumerate(folder.iterdir()):
+        b = f.parent
+        if f.suffix == '.txt': # rename only files that have the .txt extension
+            f = f.rename(b.joinpath('file {:04d}'.format(i) + '.txt'))
+
