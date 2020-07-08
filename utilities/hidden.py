fb6c282b73aa87f8641af1ff76730028125003b5
Arnold Caleb <asiimwearnoldcaleb@gmail.com>
2020-07-08 05:00:05 +0300

    Rename files with .jpg, .jpeg, .png extensions

--- utilities/hidden.py
+++ utilities/hidden.py
@@ -2,17 +2,29 @@
 import os
 from pathlib import Path
 
+# Default folders containing the files to be renamed
+TEST, TRAIN = 'datasets/test', 'datasets/train'
+
 '''
 Navigate to the directory containing the files that contain files that need to be renamed
 from the current working directory
 '''
-current_dir = Path.cwd().joinpath('files')
+def rename_files(folder_path):
+    current_dir = Path.cwd().parent
+    split_dir = current_dir.joinpath(folder_path)
+    #iterate over the folders that contain the files that need to be renamed
 
-#iterate over the folders that contain the files that need to be renamed
+    for folder in split_dir.iterdir():
+        for i, f in enumerate(folder.iterdir()):
+            b = f.parent
+            if f.suffix == '.jpeg': # rename files that have the .jpeg extension
+                f = f.rename(b.joinpath('{:04d}'.format(i) + '.jpeg'))
+            elif f.suffix == '.jpg': # rename files that have .jpg extension
+                f = f.rename(b.joinpath('{:04d}'.format(i) + '.jpg'))    
+            elif f.suffix == '.png': # rename files that have .png extension
+                f = f.rename(b.joinpath('{:04d}'.format(i) + '.png'))
+            else:
+                continue
 
-for folder in current_dir.iterdir():
-    for i, f in enumerate(folder.iterdir()):
-        b = f.parent
-        if f.suffix == '.jpeg': # rename only files that have the .txt extension
-            f = f.rename(b.joinpath('{:04d}'.format(i) + '.jpeg'))
-
+rename_files(TEST)
+rename_files(TRAIN)
